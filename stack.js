84c183e4415c2be0e6d0c690c080a848391c8fff
Kim taenam <cjb940103@gmail.com>
2021-10-19 00:45:44 +0900

    메서드 없이 스택 구현

--- stack.js
+++ stack.js
@@ -1,12 +1,13 @@
 class Stack {
     constructor() {
         this.arr = [];
+        this.index = 0;
     }
     push(item) {
-        this.arr.push(item);
+        this.arr[this.index++] = item;
     }
     pop() {
-        return this.arr.pop();
+        return this.arr[--this.index];
     }
     peek() {
         return this.arr[this.arr.length - 1];
@@ -14,9 +15,10 @@ class Stack {
 }
   
 const stack = new Stack();
-stack.push(1); // Stack {arr: Array(1)}
-stack.push(2); // Stack {arr: Array(2)}
-stack.push(3); // Stack {arr: Array(3)}
+stack.push(1); // Stack {arr: Array(1), index: 1}
+stack.push(2); // Stack {arr: Array(2), index: 2}
+stack.push(3); // Stack {arr: Array(3), index: 3}
 
 stack.pop(); // 3
+stack.pop(); // 2
 stack.peek(); // 2
