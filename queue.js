caf8f556416160bdd926bebdb651e2735d3d2aee
Kim taenam <cjb940103@gmail.com>
2021-10-19 01:13:32 +0900

    메서드 없이 큐 구현

--- queue.js
+++ queue.js
@@ -1,15 +1,17 @@
 class Queue {
     constructor() {
-      this.arr = []
+        this.arr = [];
+        this.tail = 0;
+        this.head = 0;
     }
-    enqueue(value) {
-        this.arr.push(value)
+    enqueue(item) {
+        this.arr[this.tail++] = item;
     }
     dequeue() {
-        return this.arr.shift()
-    }
-    peek() {
-        return this.arr[0]
+        if(this.head === this.tail) {
+            return null;
+        }
+        return this.arr[this.head++];
     }
 };
 
@@ -19,5 +21,7 @@ queue.enqueue(7); // arr: [7]
 queue.enqueue(33); // arr: [7, 33]
 queue.enqueue(15); // arr: [7, 33, 15]
 
-queue.dequeue() // 7
-queue.peek() // 33
+queue.dequeue(); // 7
+queue.dequeue(); // 33
+queue.dequeue(); // 15
+queue.dequeue(); // null
